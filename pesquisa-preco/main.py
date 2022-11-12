6884d3c64d40ec1b1e031ff57e263e9c57b4162f
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-11-12 20:59:01 -0300

    Update main.py

--- pesquisa-preco/main.py
+++ pesquisa-preco/main.py
@@ -9,3 +9,10 @@ def pesquisa():
             'AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/51.0.2704.103 Safari/537.36'
         }
+    
+    pqs = input("Produto: ")
+    url = 'https://www.zonasul.com.br/'+pqs
+
+    print(url)
+
+pesquisa()
