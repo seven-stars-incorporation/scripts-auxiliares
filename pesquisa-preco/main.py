5091cc740cba442d37a865f4ab5be331f30b0001
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-11-12 20:53:21 -0300

    Update main.py

--- pesquisa-preco/main.py
+++ pesquisa-preco/main.py
@@ -1,2 +1,11 @@
 # Pesquisa de preço de produtos do mercado.
 # Preciso de Header, converter a stinrg pra url válida, fazer a pesquisa... éh isso
+import requests
+from bs4 import BeautifulSoup
+
+def pesquisa():
+    header = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/51.0.2704.103 Safari/537.36'
+        }
