6c78f9bd923cf7d70db4435701e8d2a427ebd41e
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-11-14 20:52:28 -0300

    Update main.py

--- pesquisa-preco/main.py
+++ pesquisa-preco/main.py
@@ -16,21 +16,33 @@ def pesquisa():
     resp = requests.get(url, headers=header)
 
     if resp.status_code == 200:
-        
-        soup = BeautifulSoup(resp.text, 'html.parser')
 
-        produto_tag = soup.find('article')
+        print("\nConexão OK\n")
+        try:
+            soup = BeautifulSoup(resp.text, 'html.parser')
 
-        produto_preco = produto_tag.find(class_='zonasul-zonasul-store-0-x-currencyContainer')
-        produto_preco = produto_preco.get_text()
+            produto_tag = soup.find('article')
+            produto_nome = produto_tag.find('span')
+            produto_nome = produto_nome.get_text()
 
-        produto_preco = produto_preco.replace("R$", "")
-        # produto_preco = float(produto_preco)
+        except:
+            print("Erro ao pegar o nome do produto")
+            exit()
 
-        # print(type(produto_preco))
+        try:
+            produto_preco = produto_tag.find(class_='zonasul-zonasul-store-0-x-currencyContainer')
+            produto_preco = produto_preco.get_text()
 
-        produto_nome = produto_tag.find('span')
-        produto_nome = produto_nome.get_text()
+            produto_preco = produto_preco.replace("R$", "")
+            produto_preco = produto_preco.replace(",", ".")
+
+            produto_preco = float(produto_preco)
+
+        except:
+            print("Erro ao pegaro o preço do produto")
+            exit()
+
+        print(type(produto_preco))
 
         print("\n"+"="*15+"\n"+produto_nome+"\n"+str(produto_preco)+"\n")
 
